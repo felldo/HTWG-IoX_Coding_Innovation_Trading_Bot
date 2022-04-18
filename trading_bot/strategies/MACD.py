80fb72337f2afcb41fffaca24e31f485a436632a
marco <ma161mol@htwg-konstanz.de>
2022-04-18 11:33:42 +0200

    Added Comments for clarity

--- trading_bot/strategies/MACD.py
+++ trading_bot/strategies/MACD.py
@@ -1,27 +1,22 @@
 # Andere strategie
-import pprint
-from collections import deque
-from pathlib import Path
-
 import pandas as pd
 import numpy as np
 from pandas import DataFrame, Series
-
 from trading_bot.strategies.StrategyReturnType import *
-from datetime import datetime
-from dateutil import tz
 
 
 class MACD:
+    # Create prev_row variable for saving the row of the last iteration
     prev_row: Series
     # Set intersection to 0 at the start
     intersection = 0
+    # Set Oldtime to 0 at the start
     old_time = 0
     # Most Popular Combination of fast/slow length and signal values
     fast_length = 12
     slow_length = 26
     signal_line = 9
-    #                   Epoch Time,  Open_Price to Epoch Time, Close_Price, Bought/Sold Flag
+    #                                                       Open_Time , Close_Price, Bought/Sold Flag, Intersection Flag
     close_price_list_pd: DataFrame = pd.DataFrame(columns=['open_time', 'close_price', 'bought/sold', 'intersection'])
 
     # Returns current close_price_list
@@ -46,19 +41,18 @@ class MACD:
         exp1 = price['close_price'].ewm(span=self.fast_length, adjust=False).mean()
         # Exp1, Alpha = 2 / (26 + 1) = 0,074
         exp2 = price['close_price'].ewm(span=self.slow_length, adjust=False).mean()
-        # Set Frames for Panda
+        # Set Frames for new DataFrame
+        macd = pd.DataFrame(exp1 - exp2).rename(columns={'close_price': 'macd'})
         open_time = pd.DataFrame(data=self.close_price_list_pd['open_time']).rename(columns={0: 'open_time'})
         # open_time = self.close_price_list_pd['open_time']
         bought_sold = pd.DataFrame(data=self.close_price_list_pd['bought/sold']).rename(columns={0: 'bought/sold'})
         intersection = pd.DataFrame(data=self.close_price_list_pd['intersection']).rename(columns={0: 'intersection'})
-        macd = pd.DataFrame(exp1 - exp2).rename(columns={'close_price': 'macd'})
         signal = pd.DataFrame(macd.ewm(span=self.signal_line, adjust=False).mean()).rename(columns={'macd': 'signal'})
         hist = pd.DataFrame(macd['macd'] - signal['signal']).rename(columns={0: 'hist'})
-        # Connect Frame to new Panda DataFrame
+        # Connect Frames to new Panda DataFrame
         frames = [open_time, macd, signal, hist, bought_sold, intersection]
-        df = pd.concat(frames, join='inner', axis=1, ignore_index=True)
-        # print(df)
-        return df
+        full_DataFrame = pd.concat(frames, join='inner', axis=1, ignore_index=True)
+        return full_DataFrame
 
     # Implements the MACD trading strategyx
     def trade(self, open_time, close_price):
@@ -66,61 +60,44 @@ class MACD:
         self.add_kline_to_list(open_time, close_price)
         # Get Data of Moving Average Convergence/Divergence
         data = self.get_macd(self.get_data())
-        # print(type(data))
-        # print("+++++++++++++++++++++++++++++++++++++++++++++")
-        # for index, (_,row) in enumerate(data.iterrows()):
-        # print(index)
-        #print("NEW ITERATION")
-
         for index, (_, row) in enumerate(data.iterrows()):
-            # Sell if MACD > Singal and intersection
-            # print("DATA", data)
-            #print("INDEX: ", index)
-            # print("UNKNOWN: ",_)
-            # print(data.loc[i]['macd'])
-            # print(i, row)
-            # print(row)
-            # print("++++++++++++++++++++++")
-            # print(self.close_price_list_pd['bought/sold'])
-            # ("--------------------")
-            # print(self.close_price_list_pd['bought/sold'][index])
-
+            # Buy if MACD > Singal and intersection
             if row[1] > row[2]:
-                # nicht gekauft und noch nicht geschnitten
+                # If it isn't bought and isn' an intersection and the previous row is no intersection go on
                 if row[4] == 0 and row[5] == 0 and self.prev_row[5] != 2:
-                    # wenn der vorherige eine intersection war dann soll die intersection auf zwei gesetzt werden
+                    # If the prev row is the intersection point
                     if self.prev_row[5] == 1:
+                        # Then the new one couldn't be bought and the intersection Flag is set to 2 because it
+                        # is between two intersection
                         self.close_price_list_pd.loc[index, 'bought/sold'] = 0
                         self.close_price_list_pd.loc[index, 'intersection'] = 2
+                    # If its not a intersection point
                     else:
+                        # Then you set the Bought and intersection Flag to 1 and return Buy Signal for the Bot
                         self.close_price_list_pd.loc[index, 'bought/sold'] = 1
                         self.close_price_list_pd.loc[index, 'intersection'] = 1
-                        print("Bought at {}".format(open_time))
+                        #print("Bought at {}".format(open_time))
                         return StrategyReturnType.BUY
+                # Else we are just between two intersections, so set the Flag to 2
                 else:
                     self.close_price_list_pd.loc[index, 'intersection'] = 2
-                    # Marks that the Two Lines of MACD and Signal had an Intersection
-                    #self.intersection = 1
-                    # Append a Bought Flag
-
-                    #if self.prev_row[4] != 0:
-                    #    self.close_price_list_pd.loc[index, 'intersection'] = 1
-            # Sell if Singal > MACD and intersection
+            # Else Sell if Singal > MACD
             elif row[2] > row[1]:
+                # If the current isn't bougth and the prev row has an intersection Flag set to 2
                 if row[4] == 0 and self.prev_row[5] == 2:
-                    # Marks that the Two Lines of MACD and Signal had an Intersection
-                    #self.intersection = -1
-                    # Append a Sold Flag
+                    # Append a Sold Flag and that it is a intersection and return Sell Signal for the Bot
                     self.close_price_list_pd.loc[index, 'intersection'] = 1
                     self.close_price_list_pd.loc[index, 'bought/sold'] = 2
                     print("Sold at {}".format(open_time))
                     return StrategyReturnType.SELL
+            # After one full iteration set the current Row to the Previous_row so we can check both Rows
+            # in the next iteration
             self.prev_row = row
         #f = open("test.txt", "w")
         #f.write(data.to_string())
         #f.close()
-        print(data.to_string())
-
+        #print(data.to_string())
+        # If you can't sell or buy you just Hold
         return StrategyReturnType.HOLD
 
 
@@ -128,5 +105,5 @@ if __name__ == '__main__':
     macd = MACD()
     # macd.add_kline_to_list(10000, 100)
     # macd.get_data()
-    open_prices = pd.DataFrame(data=macd.close_price_list_pd)
-    macd.get_macd(open_prices['open_price'])
+    #open_prices = pd.DataFrame(data=macd.close_price_list_pd)
+    macd.get_macd(macd.close_price_list_pd)
