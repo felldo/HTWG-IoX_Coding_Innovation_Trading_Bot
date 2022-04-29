91cdc9ba7e1ec4b5c9a9ca50d1efea04e7b53124
Dominic Fellbaum <d.fellbaum@hotmail.de>
2022-04-29 17:49:38 +0200

    Try fixing cord headers

--- IoX_Coding_Innovation/settings.py
+++ IoX_Coding_Innovation/settings.py
@@ -48,14 +48,13 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ALLOWED_HOSTS=['*']
+ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 
 
